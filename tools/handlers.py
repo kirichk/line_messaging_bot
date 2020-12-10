05b97b5bf8278fb717037f9e66eb2c9c0c85d95e
Nikolai <kirichk@gmail.com>
2020-12-10 10:36:55 +0200

    another translation update

--- tools/handlers.py
+++ tools/handlers.py
@@ -831,7 +831,7 @@ def message_handler(event, stage, current_user, text):
             'phone': phone,
             'sex': sex,
             'comment': comment,
-            'country_code' = 'TH'
+            'country_code' = 'TH',
             'campaign_id':'967987',
             'sid1':'5901',
             'redirect_url':'https://line.me/en/'
